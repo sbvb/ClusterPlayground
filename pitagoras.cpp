aca20b4d3e5ebbecbd6bfe15051160ee874c78d3
alexsilva <alex.paivasilva@poli.ufrj.br>
2016-02-20 11:08:16 -0200

    deleting to update a newer version

--- pitagoras.cpp
+++
@@ -1,6 +0,0 @@
-#include "pitagoras.h"
-#include <math.h>
-
-double pitagoras(double x1, double x2, double y1, double y2){
-    return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-}
--- pitagoras.h
+++
@@ -1,7 +0,0 @@
-#ifndef PITAGORAS
-#define PITAGORAS
-
-double pitagoras(double x1, double x2, double y1, double y2);
-
-#endif // PITAGORAS
-
